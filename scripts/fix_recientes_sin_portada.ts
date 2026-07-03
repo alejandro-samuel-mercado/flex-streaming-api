bd234d5db4805bec6e862ebe34cc11966f5200fa
ale <42730179@fi.unju.edu.ar>
2026-07-03 02:04:56 -0300

    fix(script): add dotenv config to fix_recientes_sin_portada

--- scripts/fix_recientes_sin_portada.ts
+++ scripts/fix_recientes_sin_portada.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import * as dotenv from 'dotenv';
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
 
 const prisma = new PrismaClient();
 
