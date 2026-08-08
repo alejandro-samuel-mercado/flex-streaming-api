41e7cc4b2ea3bc35902ae9c808a3a1b84bac1eea
ale <42730179@fi.unju.edu.ar>
2026-08-08 12:37:18 -0300

    script: fix syntax error

--- detectar_vacios.ts
+++ detectar_vacios.ts
@@ -60,10 +60,7 @@ async function main() {
 
     if (missingContentIds.size > 0) {
         console.log('\n📝 Actualizando base de datos...');
-        await prisma.$executeRawUnsafe(`UPDATE "Content" SET "hasMissingFiles" = true WHERE id IN (${Array.from(missingContentIds).map(id => `'${id}'`).join(",")})`); // await prisma.content.updateMany({
-            // where: { id: { in: Array.from(missingContentIds) } },
-            // data: { hasMissingFiles: true }
-        });
+        await prisma.$executeRawUnsafe(`UPDATE "Content" SET "hasMissingFiles" = true WHERE id IN (${Array.from(missingContentIds).map(id => `'${id}'`).join(",")})`);
         console.log('✅ Base de datos actualizada. Ahora puedes verlos en el panel.');
     } else {
         console.log('✅ No se encontraron videos vacíos en este servidor.');
